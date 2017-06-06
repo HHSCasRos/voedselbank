322f86dc065f1499ef15fbc8a8fb35a9583a9f94
Cas Ros <16094190@student.hhs.nl>
2017-06-06 15:58:40 +0200

    homescreen toegevoegd

--- voedselbankSysteem/src/voedselbanksysteem/VoedselbankSysteem.java
+++ voedselbankSysteem/src/voedselbanksysteem/VoedselbankSysteem.java
@@ -1,6 +1,14 @@
 package voedselbanksysteem;
+
+import javax.swing.JFrame;
+
 public class VoedselbankSysteem {
     public static void main(String[] args) {
+        JFrame frame = new HomeScreen();
         
+        frame.setTitle("Home Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
     }
 }
