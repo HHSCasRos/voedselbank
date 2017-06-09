3a6765ba7510ed14bb37aed205a4fe2229070a0f
Cas Ros <16094190@student.hhs.nl>
2017-06-09 14:44:20 +0200

    changed tablename

--- voedselbankSysteem/src/voedselbanksysteem/JDBCDriver.java
+++ voedselbankSysteem/src/voedselbanksysteem/JDBCDriver.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package voedselbanksysteem;
 
 import java.sql.Connection;
@@ -23,7 +18,7 @@ public class JDBCDriver {
             Connection connection;
             connection = getConnection();
             Statement sql = connection.createStatement();
-            String query = "INSERT INTO disco (";
+            String query = "INSERT INTO temp (";
             for(int i = 0; i < velden.size(); i++){
                 query += velden.get(i);
             }
